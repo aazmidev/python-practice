bda757f8ed4ee639f4d07cade526eb4e42e3c024
Aazmi <khanum3377@gmail.com>
2026-01-18 17:30:24 +0530

    Update audit_task_2.py

--- audit_task_2.py
+++ audit_task_2.py
@@ -1,5 +1,5 @@
 number=int(input("Enter a number: "))
-for i in range(1,n+1):
+for i in range(1,number+1):
   if i%3==0 and i%5==0:
     print("FizzBuzz")
   elif i%3==0:
@@ -8,3 +8,19 @@ for i in range(1,n+1):
     print("Buzz")
   else :
     print(i)
+
+#using functions 
+number=int(input("Enter a number: "))
+def fizzbuzz(i):
+  if i%3==0 and i%5==0:
+    return "FizzBuzz"
+  elif i%3==0:
+    return "Fizz"
+  elif i%5==0:
+    return "Buzz"
+  else : 
+    return i
+for x in range(1,number+1):
+  print(fizzbuzz(x))
+
+  
